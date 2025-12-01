4a02571e0ff9f969750936cf631c19994ce47e65
carlo-baron <carlobaron430@gmail.com>
2025-12-01 15:30:10 +0800

    refactor: moved the components to a components/ folder and renamed prefix each with Sand since its for the sand sim

---
+++ src/components/SandCell.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import {
+  useState,
+  useEffect
+} from 'react';
+
+interface CellProps{
+  state: boolean;
+  row: number;
+  col: number;
+  canFall: (row, col) => number;
+  fall: (row, col) => void;
+  onClick: (row, col) => void;
+}
+
+export default function SandCell({
+  state,
+  canFall,
+  row,
+  col,
+  fall,
+  onClick,
+} : 
+  CellProps
+){
+  const [clickCd, setClickCd] = useState<boolean>(false);
+  useEffect(() => {
+    if(state === 1){
+      if(canFall(row, col)){
+        setTimeout(() => {
+          fall(row, col);
+        }, 100);
+      }
+    }
+  },[state]);
+
+  function handleClick(){
+    if(clickCd) return;
+    onClick(row, col);
+    setClickCd(true);
+
+    setTimeout(() => setClickCd(false), 300);
+  }
+
+  return(
+    <div 
+      className="bg-bluee-500 w-[1rem] aspect-1/1 outline-solid outline-white"
+      style={{
+        background: state ? 'white' : '',
+      }}
+      onClick={handleClick}
+    >
+    </div>
+  );
+}
---
+++ src/components/SandGrid.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import {
+  useState,
+  useEffect
+} from 'react';
+import SandCell from './SandCell';
+
+export default function SandGrid({size} : {size: number;}){
+  const [cells, setCells] = useState<number[][]>([]);
+  useEffect(() => {
+    const nextCells: number[][] = [];
+    for(let i = 0; i < size; i++){
+      nextCells[i] = [];
+      for(let j = 0; j < size; j++){
+        nextCells[i][j] = 0;
+      }
+    }
+    setCells(nextCells);
+  }, [size]);
+
+  function neighbors(row: number, col: number) {
+    const dirs = [
+      [-1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, -1],
+      [-1, -1],
+    ];
+
+    const neighborCells: number[] = [];
+
+    for (const [dr, dc] of dirs) {
+      const r = row + dr;
+      const c = col + dc;
+
+      if (r >= 0 && r < cells.length && c >= 0 && c < cells[0].length) {
+        neighborCells.push(cells[r][c]);
+      } else {
+        neighborCells.push(null);
+      }
+    }
+
+    return neighborCells;
+  }
+
+  function getNeighbor(row, col){
+    const neighborCells = neighbors(row, col);
+    return neighborCells.filter(neighbor => neighbor === 1).length;
+  }
+
+  function canFall(row, col){
+    const neighborCells = neighbors(row, col);
+    return neighborCells[3] === 0 ||
+           neighborCells[4] === 0 ||
+           neighborCells[5] === 0;
+  }
+
+  function fall(row, col){
+    const nextCells = [...cells];
+    const neighborCells = neighbors(row, col);
+    
+    nextCells[row][col] = 0;
+    if(neighborCells[4] === 0){
+      nextCells[row+1][col] = 1;
+    }else if(neighborCells[3] === 0){
+      nextCells[row+1][col+1] = 1;
+    }else if(neighborCells[5] === 0){
+      nextCells[row+1][col-1] = 1;
+    }
+
+    setCells(nextCells);
+  }
+
+  function activateCell(row, col){
+    const nextCells = [...cells];
+    nextCells[row][col] = 1;
+    setCells(nextCells);
+  }
+
+  return(
+    <div
+    className='gap-1 grid'
+    style={{
+      gridTemplateColumns: `repeat(${size}, 1fr)`,
+      gridAutoRows: '1fr',
+    }}
+    >
+      {
+        cells.flat().map((cell, index) => {
+          const row = Math.floor(index / size);
+          const col = index % size;
+
+          return(
+            <SandCell 
+            key={index}
+            row={row}
+            col={col}
+            state={cell}
+            canFall={canFall}
+            fall={fall}
+            onClick={activateCell}
+            />
+          );
+        })
+      }
+    </div>
+  );
+}
