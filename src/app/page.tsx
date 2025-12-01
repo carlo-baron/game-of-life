14e1bc1890ffb24478632612a22ae09753bc65ab
carlo-baron <carlobaron430@gmail.com>
2025-12-01 14:45:31 +0800

    feat: falling block
    note: i decided to progress to game of life instead, i will play around make falling block effect adding block in runtime and so on before i do it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,6 +35,50 @@ function Grid({size} : {size: number;}){
     setCells(nextCells);
   }, [size]);
 
+  function neighbors(row: number, col: number) {
+    const dirs = [
+      [-1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, -1],
+      [-1, -1],
+    ];
+
+    const neighborCells: number[] = [];
+
+    for (const [dr, dc] of dirs) {
+      const r = row + dr;
+      const c = col + dc;
+
+      if (r >= 0 && r < cells.length && c >= 0 && c < cells[0].length) {
+        neighborCells.push(cells[r][c]);
+      } else {
+        neighborCells.push(null);
+      }
+    }
+
+    return neighborCells;
+  }
+
+  function getNeighbor(row, col){
+    const neighborCells = neighbors(row, col);
+    return neighborCells.filter(neighbor => neighbor === 1).length;
+  }
+
+  function canFall(row, col){
+    return neighbors(row, col)[4] === 0;
+  }
+
+  function fall(row, col){
+    const nextCells = [...cells];
+    nextCells[row][col] = 0;
+    nextCells[row+1][col] = 1;
+    setCells(nextCells);
+  }
+
   return(
     <div
     className='gap-1 grid'
@@ -44,17 +88,51 @@ function Grid({size} : {size: number;}){
     }}
     >
       {
-        cells.flat().map((cell, index) => <Cell 
-                  key={index}
-                  state={cell}
-                  />)      
+        cells.flat().map((cell, index) => {
+          const row = Math.floor(index / size);
+          const col = index % size;
+
+          return(
+            <Cell 
+            key={index}
+            row={row}
+            col={col}
+            state={cell}
+            canFall={canFall}
+            fall={fall}
+            />
+          );
+        })
       }
     </div>
   );
 }
 
-function Cell({state, onDetect} : {state: CellProps; onDetect: () => void}){
-  useEffect(() => {},[]);
+function Cell({
+  state,
+  canFall,
+  row,
+  col,
+  fall,
+} : 
+  {
+  state: CellProps;
+  row: number;
+  col: number;
+  canFall: (row, col) => number;
+  fall: (row, col) => void;
+}){
+  useEffect(() => {
+    if(state === 1){
+      console.log(row, col);
+      if(canFall(row, col)){
+        setTimeout(() => {
+          fall(row, col);
+        }, 100);
+      }
+    }
+  },[state]);
+
   return(
     <div 
       className="bg-bluee-500 w-[1rem] aspect-1/1 outline-solid outline-white"
